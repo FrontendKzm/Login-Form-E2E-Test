3609bbb34c9a681125fc0fbfeaca2a818c6be3da
kaizm51 <kzbwrtf@gmail.com>
2025-02-25 20:10:59 +0300

    more changes...

--- Login-Form-Test/src/App.jsx
+++ Login-Form-Test/src/App.jsx
@@ -1,23 +1,13 @@
 import { useState } from 'react'
-<<<<<<< HEAD
 import Login from '../components/Login'
 import Success from '../components/Success'
-=======
-
->>>>>>> 13e29de8b2094ba48cdb335b9979f386c93528f1
 import './App.css'
 
 function App() {
   
-<<<<<<< HEAD
 return(
   <Login/>
 
 )}
-=======
-
-  
-}
->>>>>>> 13e29de8b2094ba48cdb335b9979f386c93528f1
 
 export default App
