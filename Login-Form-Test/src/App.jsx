b62f76505f40e852dabae82ba218fbd9601efcd6
kaizm51 <kzbwrtf@gmail.com>
2025-02-25 19:30:38 +0300

    Log In Page is done!

--- Login-Form-Test/src/App.jsx
+++ Login-Form-Test/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-
+import Login from '../components/Login'
+import Success from '../components/Success'
 import './App.css'
 
 function App() {
   
+return(
+  <Login/>
 
-  
-}
+)}
 
 export default App
